fe1f2a927b1595fa8253f9e2902783da94578184
Anton Kormakov <anton.kormakov@rosberry.com>
2024-03-18 10:51:20 +0600

    Revert "Made settings player muted"
    This reverts commit 0f017185069f37e93b7d19df5fd323547dd2f257.

--- Sources/PurchaseKitSettings/Presentation/Modules/Settings/Components/SettingsPremiumHeaderView/SettingsPremiumHeaderView.swift
+++ Sources/PurchaseKitSettings/Presentation/Modules/Settings/Components/SettingsPremiumHeaderView/SettingsPremiumHeaderView.swift
@@ -38,7 +38,6 @@ final class SettingsPremiumHeaderView: UITableViewHeaderFooterView {
         }
         playerView.backgroundColor = colorScheme.background.primary
         playerView.player = playerLooper.player
-        playerLooper.player.isMuted = true
         playerLooper.play()
 
         gradientView.gradient = .vertical(colors: [colorScheme.background.primary.withAlphaComponent(0.25),
